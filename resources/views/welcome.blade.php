f3675c0197a36278fda2154e85e1025ac745caf2
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-03-08 17:23:39 +0100

    Startseite geändert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hallo
                 </div>
 
                 <div class="links">
