c0b984aeb95388da07f76cf24ecefd7ec85bd224
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-03-09 15:52:45 +0100

    Javascript für Twitter Bootstrap nachinstalliert

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,6 +6,13 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+    require('bootstrap');
+} catch (e) {}
+
+
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
