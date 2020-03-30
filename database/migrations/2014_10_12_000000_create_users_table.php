48ea34f541db5396292e9c58d6edfe69ad7c5a30
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-03-30 19:46:50 +0200

    User erweitert

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('motto')->nullable();
+            $table->text('ueber_mich')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
