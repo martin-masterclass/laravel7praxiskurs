87b2221ade23e47530572105a01d800dc16cb3b2
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-03-30 19:50:49 +0200

    Tag index

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -6,14 +6,16 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Alle Hobbies</div>
+                    <div class="card-header">Alle Tags</div>
 
                     <div class="card-body">
                         <ul class="list-group">
-                            @foreach($hobbies as $hobby)
-                                <li class="list-group-item">{{ $hobby->name }} <a class="ml-2" href="/hobby/{{ $hobby->id }}">Detailansicht</a>
-                                    <a class="ml-2 btn btn-sm btn-outline-primary" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Bearbeiten</a>
-                                    <form style="display: inline;" action="/hobby/{{ $hobby->id }}" method="post">
+                            @foreach($tags as $tag)
+                                <li class="list-group-item">
+                                    <span style="font-size: 130%;" class="mr-2 badge badge-{{ $tag->style }}">{{ $tag->name }}</span>
+                                    ({{ $tag->style }})
+                                    <a class="ml-2 btn btn-sm btn-outline-primary" href="/tag/{{ $tag->id }}/edit"><i class="fas fa-edit"></i> Bearbeiten</a>
+                                    <form style="display: inline;" action="/tag/{{ $tag->id }}" method="post">
                                         @csrf
                                         @method('DELETE')
                                         <input class="btn btn-outline-danger btn-sm ml-2" type="submit" value="Löschen">
@@ -21,7 +23,7 @@
                                 </li>
                             @endforeach
                         </ul>
-                        <a class="btn btn-success btn-sm mt-3" href="hobby/create"><i class="fas fa-plus-circle"></i> Neues Hobby anlegen</a>
+                        <a class="btn btn-success btn-sm mt-3" href="/tag/create"><i class="fas fa-plus-circle"></i> Neuen Tag anlegen</a>
                     </div>
                 </div>
             </div>
