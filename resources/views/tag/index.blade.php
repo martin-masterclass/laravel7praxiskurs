00ad916785ec66f699cca263f8335b51399878ff
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-04-11 21:21:07 +0200

    Tag-Statistik

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -20,6 +20,7 @@
                                         @method('DELETE')
                                         <input class="btn btn-outline-danger btn-sm ml-2" type="submit" value="Löschen">
                                     </form>
+                                    <a class="float-right" href="/hobby/tag/{{ $tag->id }}">{{ $tag->hobbies->count() }} mal verwendet</a>
                                 </li>
                             @endforeach
                         </ul>
