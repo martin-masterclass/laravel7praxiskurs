5017ac718ab8676d0ebd7be6e664ee3258be1f59
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-04-26 00:36:21 +0200

    Gäste mit der Auth-Middleware steuern (Hobbies)

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -11,6 +11,12 @@ use Intervention\Image\Facades\Image;
 
 class HobbyController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
